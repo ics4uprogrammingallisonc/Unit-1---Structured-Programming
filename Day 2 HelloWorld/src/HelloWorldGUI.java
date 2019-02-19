2d60dd901691b97ba2233b9c3aad9742b7d51202
Allison <allison.cook@ocsbstudent.ca>
2019-02-19 12:43:33 -0500

    Initial commit

---
+++ Day 2 HelloWorld/src/HelloWorldGUI.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class HelloWorldGUI {
+
+	public static void main(String[] args) {
+		//Declare the strings
+			String wordOne = "Hello";
+			String wordTwo = "World";
+			String wordThree = "GUI";
+				
+		//add the string together
+			String message = wordOne + " " + wordTwo + " " + wordThree;
+		//Create and show the the GUI text
+			JOptionPane.showMessageDialog(null, message);
+		//add a background colour
+			
+		//add an image
+			
+	}
+
+}
---
+++ Day 2 HelloWorld/src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Created by: Allison Cook
+ * Created on: 01-Feb-2019
+ * Created for: ICS4U Programming
+ * Daily Assignment – Day #2 - Hello World
+ * This program combines two strings and displays hello world
+*/
+public class Main {
+
+	public static void main(String[] args) {
+		//Declare the strings
+		String wordOne = "Hello";
+		String wordTwo = "World!";
+		
+		//Print the strings
+		System.out.println(wordOne + " " + wordTwo);
+	}
+}
